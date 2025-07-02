b27d3790c9d8dc5866578d92eb84dca0a7ea6db5
Johannes Koch <johanneskoch@fico.com>
2025-07-02 10:14:47 +0200

    push latest test version and screenshots

--- flutter_app/integration_test/create_screenshots.dart
+++ flutter_app/integration_test/create_screenshots.dart
@@ -205,14 +205,14 @@ Future<void> _takeScreenshot({
   }
 
   // Wait a bit more for any animations to complete
-  print('Await pumpAndSettle');
+  print('Await pumpAndSettle 2');
   await tester.pumpAndSettle(const Duration(seconds: 5));
 
   try {
     // Capture screenshot
     print('Await capture');
-    await Future.delayed(const Duration(seconds: 1));
-
+    //await Future.delayed(const Duration(seconds: 1));
+    print('Await capture after delayed');
     final Uint8List? imageBytes = await controller
         .capture(
           delay: const Duration(milliseconds: 500),
